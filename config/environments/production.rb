88e4ad9a1f384688437d369da9420064a13fcdba
yutaka <yutaka@yutaka-no-MacBook-Air.local>
2014-07-13 20:28:35 +0900

    google アナリティクスを適用

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  GA.tracker = "UA-52775464-1"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
